c2572ff6346fbd3a48f6c00c973ace8599e688b4
GisfaRyangga <gisfa.putra2603@mail.ugm.ac.id>
2023-06-21 18:01:41 +0700

    icon panah

--- app/src/main/java/com/example/no1uaspapb2/CustomButton.java
+++ app/src/main/java/com/example/no1uaspapb2/CustomButton.java
@@ -5,17 +5,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.res.ResourcesCompat;
 
 public class CustomButton extends AppCompatButton {
-
-
-
     boolean mBoolean = false;
+
+    Drawable right_path, left_path;
 
     public CustomButton(Context context) {
         super(context);
@@ -23,6 +24,14 @@ public class CustomButton extends AppCompatButton {
 
     public CustomButton(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        right_path = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.ic_baseline_chevron_right_24, null);
+
+        left_path = ResourcesCompat.getDrawable(getResources(),
+                R.drawable.ic_baseline_chevron_left_24, null);
+
+        setCompoundDrawablesWithIntrinsicBounds(right_path, null, left_path, null);
 
         setBackgroundColor(Color.RED);
         setTextColor(Color.WHITE);
@@ -40,9 +49,4 @@ public class CustomButton extends AppCompatButton {
             }
         });
     }
-
-    public CustomButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
 }
